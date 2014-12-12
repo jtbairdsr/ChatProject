bb436848cb144fa6a695154092e9ffc2eedddd80
jtbairdsr <jonathan@haven.local>
2014-12-12 14:21:25 -0700

    Add disable user functionality.

--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -2,27 +2,59 @@
  * @Author: jonathan
  * @Date:   2014-12-05 14:09:33
  * @Last Modified by:   jonathan
- * @Last Modified time: 2014-12-08 14:54:39
+ * @Last Modified time: 2014-12-12 13:16:21
  */
 
 angular.module('chatApp')
-    .controller('UsersCtrl', ['$scope', '$state', '$firebase', 'currentAuth',
-        function($scope, $state, $firebase, currentAuth) {
-            var ref = new Firebase('https://luminous-inferno-5021.firebaseIO.com/users');
-            var sync = $firebase(ref);
-            var usersArray = sync.$asArray();
+    .controller('UsersCtrl', ['$scope', '$state', '$firebase', '$firebaseAuth', 'currentAuth', '$alert',
+        function($scope, $state, $firebase, $firebaseAuth, currentAuth, $alert) {
+            var usersRef = $scope.ref.child('users');
+            var usersArray = $firebase(usersRef).$asArray();
             $scope.users = usersArray;
-            $scope.newUser = {};
+            $scope.$emit('getUser');
+            $scope.newUser = {
+                admin: false,
+                disabled: false
+            };
+            var auth = $firebaseAuth($scope.ref);
+            $scope.updateUser = function(user) {
+                user.edit = false;
+                $scope.users.$save(user)
+                    .then(function(ref) {
+                        var nameChangeSuccess = $alert({
+                            title: 'Success',
+                            content: user.$id + ' has been updated',
+                            placement: 'top-right',
+                            duration: '3',
+                            type: 'success',
+                            show: true
+                        });
+                    })
+                    .catch(function(error) {
+                        var nameChangeError = $alert({
+                            title: 'Error:',
+                            content: error.toString().split(':')[1],
+                            placement: 'top-right',
+                            duration: '3',
+                            type: 'danger',
+                            show: true
+                        });
+                    });
+            };
             $scope.addUser = function() {
-            	usersArray.$add({
-            		uName: $scope.newUser.uName,
-            		fName: $scope.newUser.fName,
-            		lName: $scope.newUser.lName,
-            		password: $scope.newUser.password,
-            		admin: $scope.newUser.admin,
-            		edit: false
-            	})
-            	$scope.newUser={};
+                auth.$createUser($scope.newUser.uName, $scope.newUser.password)
+                    .then(function(user) {
+                        console.log('create user success');
+                    }, function(error) {
+                        console.log('create user failed' + error);
+                    });
+                $firebase(usersRef).$set($scope.newUser.uName.split('@')[0], {
+                    fName: $scope.newUser.fName,
+                    lName: $scope.newUser.lName,
+                    admin: $scope.newUser.admin,
+                    edit: false
+                });
+                $scope.newUser = {};
             };
         }
     ])
